42200d008119e4640cb4d8da297ec3e820cc9a32
SiLeNCeD <silenced@kloudnine.com>
2015-08-16 19:52:55 -0400

    First commit

---
+++ src/main/java/com/kloudnine/SelTesting/SelTesting.java
@@ -0,0 +1,21 @@
+package com.kloudnine.SelTesting;
+
+/**
+ * Created by Steven on 8/16/2015.
+ * Sel Testing
+ */
+public class SelTesting {
+
+    public static void main(String[] args) {
+
+        String a;
+
+        a = "Hello there!";
+
+        System.out.println( a );
+
+
+
+    }
+
+}
