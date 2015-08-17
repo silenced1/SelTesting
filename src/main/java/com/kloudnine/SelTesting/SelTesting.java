739901e288118bd1dd93c7faaad495faddb0efaf
SiLeNCeD <silenced@kloudnine.com>
2015-08-16 23:54:48 -0400

    Sel Testing step 1

--- src/main/java/com/kloudnine/SelTesting/SelTesting.java
+++ src/main/java/com/kloudnine/SelTesting/SelTesting.java
@@ -1,5 +1,11 @@
 package com.kloudnine.SelTesting;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 /**
  * Created by Steven on 8/16/2015.
  * Sel Testing
@@ -8,12 +14,30 @@ public class SelTesting {
 
     public static void main(String[] args) {
 
-        String a;
+        WebDriver driver = new FirefoxDriver();
 
-        a = "Hello there!";
+        // define URL
+        driver.get( "http://www.google.com" );
 
-        System.out.println( a );
 
+        // perform tests
+        driver.findElement(By.name( "q" )).sendKeys( "Cheese" );
+        driver.findElement(By.name( "q" )).submit();
+
+        System.out.println( "Page title is: " + driver.getTitle());
+
+        // Google's page is rendered dynamically with java
+        // so wait for page to load (timeout after 10 seconds)
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("cheese");
+            }
+        });
+
+        System.out.println( "Page title is: " + driver.getTitle());
+
+        // stop driver/close browser
+        driver.quit();
 
 
     }
